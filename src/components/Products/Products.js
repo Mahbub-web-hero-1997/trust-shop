cbf7621a99836bb524823c1e5a6a1d5b3c9fdf1e
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-03-26 13:05:13 +0600

    cart added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product'
 import './Products.css'
-import Cart from '../Cart/Cart';
+
 const Products = () => {
     const [products, setProducts] = useState([]);
-    const [product, setProduct] = useState([])
+    const [cart, setCart] = useState([]);
+    // console.log(cart);
+    const addToCart = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
-    const addToCart = (product) => {
-        setProduct(product);
-    }
+
     return (
         <div className='Container'>
             <div className='all_Products'>
+
                 {
                     products.map(product => <Product
                         key={product.id}
@@ -23,14 +27,16 @@ const Products = () => {
                         addToCart={addToCart}
                     ></Product>)
                 }
-                {
-                    product.map(item => console.log(item))
-                }
+
             </div>
             <div className='cart'>
-                <Cart></Cart>
+                {
+                    cart && cart.map(item =>
+                        <h1>name:{item.name}</h1>
+                    )
+                }
             </div>
-        </div>
+        </div >
     );
 };
 
