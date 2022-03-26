ae53da0549c37da7b61afde415a1ed83be14f669
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-03-26 13:47:51 +0600

    new added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,24 @@
+// Import File From other Component
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product'
 import './Products.css'
 
+// Products Component
 const Products = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    // console.log(cart);
+    const [clear, setClear] = useState();
     const addToCart = (product) => {
         const newCart = [...cart, product]
         setCart(newCart);
     }
+
+    const clearCartItem = () => {
+        const clearCart = [];
+        const newClearCart = [clear, clearCart]
+        setClear(newClearCart)
+    }
+    // Fetch Data From JSON File
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
@@ -19,7 +28,6 @@ const Products = () => {
     return (
         <div className='Container'>
             <div className='all_Products'>
-
                 {
                     products.map(product => <Product
                         key={product.id}
@@ -29,12 +37,15 @@ const Products = () => {
                 }
 
             </div>
+            {/* Cart Aria */}
             <div className='cart'>
+                <h4>Add Watch</h4>
                 {
                     cart && cart.map(item =>
-                        <h1>name:{item.name}</h1>
+                        <h4>name:{item.name}</h4>
                     )
                 }
+                <button onClick={() => { clearCartItem(clear) }}>Clear</button>
             </div>
         </div >
     );
